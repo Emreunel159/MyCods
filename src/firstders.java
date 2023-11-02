6c56312de39afa324cf5491109d000f731013997
Emre Ünel <emreunel159@gmail.com>
2023-11-02 21:48:25 +0300

    First code

---
+++ src/firstders.java
@@ -0,0 +1,18 @@
+/**
+ * firstders
+ */
+public class firstders {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        String ad = "Emre";
+        String soyad = "Ünel";
+        int yas = 20;
+        float sayi = 36623525326F;
+        long sayi2 = 2435353232322463L;
+        String ogrno = "2311290043";
+        System.out.println(
+                "Ad: " + ad + " " + "Soyad: " + soyad + " " + "Yaş: " + yas + " " + "Öğrenci Numarası: " + ogrno + " ");
+        System.out.println(sayi + sayi2);
+    }
+}
